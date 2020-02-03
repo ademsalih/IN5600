ed4bbb32f61535bc292b2ee9be29b92888dce583
Adem Salih <ademsal.97@icloud.com>
2020-02-03 15:50:33 +0100

    progress

---
+++ week6-activities/app/src/main/java/com/ademsalih/activities/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.ademsalih.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+
+    public void returnReply(View view) {
+
+    }
+}
