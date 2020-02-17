986234013285954f60198a8782611f42ea609829
Adem Salih <ademsal.97@icloud.com>
2020-02-17 14:27:00 +0100

    added volley project

--- ImplicitIntentsReceiver/app/src/main/java/com/ademsalih/implicitintentsreceiver/MainActivity.java
+++ ImplicitIntentsReceiver/app/src/main/java/com/ademsalih/implicitintentsreceiver/MainActivity.java
@@ -2,7 +2,10 @@ package com.ademsalih.implicitintentsreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+
+        if (uri != null) {
+            String uriString = getString(R.string.uri_label) + uri.toString();
+            TextView textView = findViewById(R.id.text_uri_message);
+            textView.setText(uriString);
+        }
     }
 }
