aca754ee23afb75c2f269fe7e2689dbff8e4c4df
Adem Salih <ademsal.97@icloud.com>
2020-02-17 15:59:49 +0100

    fixed nullpointer bug

--- VolleyExample/app/src/main/java/com/ademsalih/volleyexample/MainActivity.java
+++ VolleyExample/app/src/main/java/com/ademsalih/volleyexample/MainActivity.java
@@ -65,10 +65,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static void hideKeyboard(Activity activity) {
-        InputMethodManager inputMethodManager =
-                (InputMethodManager) activity.getSystemService(
-                        Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(
-                activity.getCurrentFocus().getWindowToken(), 0);
+        InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        View currentFocus = activity.getCurrentFocus();
+        if (currentFocus != null) {
+            inputMethodManager.hideSoftInputFromWindow(currentFocus.getWindowToken(), 0);
+        }
     }
 }
